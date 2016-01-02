506659f1d655626481380fa2176ab875b547d8d1
Ashwin Ahuja <ashwin.ahuja@gmail.com>
2016-01-02 20:30:34 +0000

    Added loads of files

---
+++ TEMP/TEMP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace TEMP
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+						using (StreamWriter writer = new StreamWriter ("i.txt")) {
+							for (int i = 33; i < 325; i++) {
+								if (i > 126 && i < 160) {
+									i = 161;
+								}
+								Console.WriteLine ("I: " + i + "\t" + (char)i);
+								writer.WriteLine ("I: " + i + "\t" + (char)i);			
+							}
+						}
+			Console.ReadLine ();
+		}
+	}
+}
